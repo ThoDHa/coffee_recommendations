90f7b4826086c2435bab18e2e5eb0f1ea3847703
Tho Ha <tho.ha@gatech.edu>
2023-05-31 00:15:35 -0500

    Initial search stuff, just stubs really

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::collections::HashMap;
-
-use cities::print_state;
 mod cities;
+mod reddit;
 
 #[tokio::main]
 async fn main() {
-    let united_states: HashMap<String, Vec<String>> = cities::get_cities().await;
-    print_state(&united_states);
+    let united_states: HashMap<String, Vec<String>> = cities::get_cities_from_file().await;
+    let subreddits: [String; 1] = ["r/coffee".to_string()];
+
+    for subreddit in subreddits.iter() {
+        for state in united_states.iter() {
+            for city in state.1.iter() {
+                reddit::get_search(subreddit.to_string(), city.to_string(), state.0.to_string());
+            }
+        }
+    }
 }
---
+++ src/reddit.rs
@@ -0,0 +1,10 @@
+use reqwest;
+
+
+pub fn get_search(subreddit: String, city: String, state: String) {
+    println!("{}, {}, {}", subreddit, city, state)
+    // https://www.reddit.com/dev/api/#get_search
+    // Use the API to search for the reddit then the city name,
+    // then city name and state name.
+    //
+}
